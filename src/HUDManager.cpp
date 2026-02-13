8a26ed3896cfb0e24483e7e53d956a22af431471
Fuzzles <fuzzmyrim@gmail.com>
2026-02-13 13:33:35 -0800

    F

--- src/HUDManager.cpp
+++ src/HUDManager.cpp
@@ -1000,7 +1000,7 @@ void HUDManager::ApplyHUDMenuSpecifics(RE::GPtr<RE::GFxMovieView> a_movie, float
 			// TESGlobal in esp ensures compass is always hidden if set.
 			if (isCompass && !compat->IsCompassAllowed()) {
 				dInfo.SetVisible(false);
-				dInfo.SetAlpha(0.0);
+				dInfo.SetAlpha(0.0f);
 				elem.SetDisplayInfo(dInfo);
 				continue;
 			}
